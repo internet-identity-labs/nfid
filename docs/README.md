746c17150f408229c569a8ee646e19415bb6f9f9
Philipp Litzenberger <philipp@identitylabs.ooo>
2023-11-29 11:32:07 +0100

    feat([sc-9863]): init docs and readme

---
+++ docs/README.md
@@ -0,0 +1,1 @@
+# NFID Open Source Documentation
---
+++ docs/theme.config.tsx
@@ -0,0 +1,23 @@
+const REPO_BASE = 'https://github.com/internet-identity-labs/nfid'
+const theme = {
+  logo: (
+    <div>
+      <strong>@nfid</strong>/identity-kit
+    </div>
+  ),
+  docsRepositoryBase: REPO_BASE,
+  project: {
+    link: REPO_BASE,
+  },
+  footer: {
+    text: (
+      <span>
+        © {new Date().getFullYear() + ' '}
+        Internet Identity Labs.
+      </span>
+    ),
+  },
+  // ... other theme options
+}
+
+export default theme
