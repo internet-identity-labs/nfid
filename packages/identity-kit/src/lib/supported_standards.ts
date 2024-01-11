83c9e7755404e21805a9191d7df3c79c86e7759c
Philipp Litzenberger <philipp@identitylabs.ooo>
2024-01-11 14:40:30 +0100

    feat([sc-9867]): define base folder structure

---
+++ packages/identity-kit/src/lib/supported_standards.ts
@@ -0,0 +1,9 @@
+export enum SupportedStandards {
+  "ICRC_25" = "ICRC-25",
+  "ICRC_31" = "ICRC-31",
+  "ICRC_32" = "ICRC-32",
+  "ICRC_33" = "ICRC-33",
+  "ICRC_34" = "ICRC-34",
+}
+
+export type SupportedStandardsType = Array<keyof typeof SupportedStandards>;
---
+++ packages/identity-kit/src/standars/icrc-31/index.ts
@@ -0,0 +1,1 @@
+// TODO: Define the ICRC-31 standard typescript Interface
