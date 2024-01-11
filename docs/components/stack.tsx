e61b07d30b98368d722c4e7f3c0e79ce5c6d083d
Philipp Litzenberger <philipp@identitylabs.ooo>
2024-01-11 13:19:03 +0100

    feat([sc-9893]): types for stack component

--- docs/components/stack.tsx
+++ docs/components/stack.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
-export const Stack = ({ children }: { children: React.ReactNode | React.ReactNode[] }) => (
+export const Stack = ({ children }: { children: JSX.Element }) => (
   <div className="flex gap-2 mt-4 mb-8">
-    {children && React.Children.map(children, (child: React.ReactNode) => <div>{child}</div>)}
+    {children && React.Children.map(children, (child) => <div>{child}</div>)}
   </div>
 )
