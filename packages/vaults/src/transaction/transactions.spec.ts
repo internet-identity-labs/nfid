b4ebb120b4a1ced4134912995d9859059125feae
olsemeno <sendtosemenov@gmail.com>
2024-03-18 18:18:58 +0100

    Try first vault

--- packages/vaults/src/transaction/transactions.spec.ts
+++
@@ -1,9 +0,0 @@
-import {registerMappers, TransactionMapperRegistry} from "./mapper_registry";
-
-describe("Mappers", () => {
-    it("log mappers", () => {
-        registerMappers()
-        let mappers = TransactionMapperRegistry.size;
-        console.log(mappers)
-    })
-})
