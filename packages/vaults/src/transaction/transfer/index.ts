7a6f101e32ebbb3e2701be9497d44e6c0826ecaa
olsemeno <sendtosemenov@gmail.com>
2024-03-29 14:34:16 +0100

    chore(vaults): release v0.0.3-alpha.14

--- packages/vaults/src/transaction/transfer/index.ts
+++ packages/vaults/src/transaction/transfer/index.ts
@@ -1,3 +1,4 @@
 export {type TopUpTransaction, TopUpTransactionRequest} from "./top_up"
 export {type TransferTransaction, TransferTransactionRequest} from "./transfer"
 export {type TransferQuorumTransaction, TransferQuorumTransactionRequest} from "./transfer_quorum"
+export {type TransferICRC1QuorumTransaction, TransferICRC1QuorumTransactionRequest} from "./transfer_icrc1_quorum"
