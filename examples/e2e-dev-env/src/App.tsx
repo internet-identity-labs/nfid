ff7abffc7ac91731078eb87a5fcdd1ad4973db99
Dmitrii <dmitrii@identityLabs.ooo>
2024-04-25 16:29:13 +0400

    identity kit Demo Implement the CI CD for [sc-12215]

--- examples/e2e-dev-env/src/App.tsx
+++ examples/e2e-dev-env/src/App.tsx
@@ -2,17 +2,20 @@ import { Flex, Heading } from "@radix-ui/themes"
 import { Header } from "./components/header"
 
 function App() {
+  const dapp = import.meta.env.VITE_DAPP_CANISTER_ID
+  const signer = import.meta.env.VITE_SIGNER_CANISTER_ID
+
   return (
     <Flex direction="column" height={"100%"}>
       <Header />
       <Flex gap="2" height={"100%"}>
         <Flex direction="column" grow="1" p={"4"} gap="4">
           <Heading as="h2">Example dApp</Heading>
-          <iframe src="http://localhost:3001/" className="h-full" />
+          <iframe src={dapp} className="h-full" />
         </Flex>
         <Flex direction="column" grow="1" p={"4"} gap="4">
           <Heading as="h2">Example Signer</Heading>
-          <iframe src="http://localhost:3002/" className="h-full" />
+          <iframe src={signer} className="h-full" />
         </Flex>
       </Flex>
     </Flex>
