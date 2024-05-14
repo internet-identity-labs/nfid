370ebb3d53393e8ec4798d59de95011030b16a76
Dmitrii <105656449+dmitriiIdentityLabs@users.noreply.github.com>
2024-05-14 14:49:01 +0400

    identity kit demo Remove e2e dev env canisters [sc-12591] (#160)

--- examples/e2e-dev-env/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "@jest/types"
-
-const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testPathIgnorePatterns: ["dist/", "tests", "tests-examples"],
-}
-
-export default config
