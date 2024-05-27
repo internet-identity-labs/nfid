0b23fe7334a3ee8324402fe1e548b1b8113807db
Pavlo Chorniy <official.chorniy@gmail.com>
2024-05-27 16:32:28 +0200

    fix: remove duplicated test

--- examples/react-dapp/tests/icrc25.spec.ts
+++ examples/react-dapp/tests/icrc25.spec.ts
@@ -57,25 +57,4 @@ test.describe("icrc25", () => {
       expect(responseSection).toContainText(`"method": "icrc27_get_accounts"`)
     })
   })
-
-  test("icrc25_granted_permissions", async ({ page }) => {
-    await test.step("icrc25_request_permissions", async () => {
-      await getPermissions(page)
-    })
-
-    await test.step("icrc25_granted_permissions", async () => {
-      const sectionId = "icrc25_granted_permissions"
-
-      await verifySectionVisibility(page, sectionId)
-      await verifyRequestSection(page, sectionId, icrc25GrantedRequest)
-      await verifyResponseSection(page, sectionId, "{}")
-
-      await submitRequest(page, sectionId)
-      await page.waitForTimeout(1000)
-      const responseSection = page.locator(`#${sectionId} #response-section-e2e`)
-      expect(responseSection).toContainText(`"origin": "${origin}"`)
-      expect(responseSection).toContainText(`"scopes": [`)
-      expect(responseSection).toContainText(`"method": "icrc27_get_accounts"`)
-    })
-  })
 })
